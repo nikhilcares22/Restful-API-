5a85281955bf9deeb39ee851d0ce562580d49952
nikhilcares22@gmail.com <nikhilcares22@gmail.com>
2020-09-19 13:36:43 +0530

    deleted files

--- api/middleware/checkAuth.js
+++ api/middleware/checkAuth.js
@@ -3,8 +3,9 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1];
-
+        // console.log(token);
         const decoded = jwt.verify(token, process.env.JWT_KEY);
+        // console.log(decoded)
         res.userData = decoded;
         next();
     } catch (err) {
