76c2b775db78e82ade83ff98d89c68c55fb709b0
nikhilcares22@gmail.com <nikhilcares22@gmail.com>
2020-09-20 12:57:51 +0530

    done

--- api/middleware/checkAuth.js
+++ api/middleware/checkAuth.js
@@ -4,11 +4,9 @@ module.exports = (req, res, next) => {
     try {
         console.log("body", req.body);
         const decoded = jwt.verify(req.body.token, process.env.JWT_KEY);
-        // req.userData = decoded;
-        console.log(decoded);
+        req.userData = decoded;
         next();
     } catch (error) {
-        console.log("hereee");
         return res.status(401).json({
             message: 'Auth Faileed'
         });
